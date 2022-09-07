d5ac177b8bb7c55ef2fa4229c6022f089688233e
Denis Badurina <denis@domonda.com>
2022-09-07 15:45:01 +0200

    graphql-helix

---
+++ implementations/graphql-helix/index.mjs
@@ -0,0 +1,52 @@
+import {
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLObjectType,
+  assertWrappingType,
+} from 'graphql';
+import {
+  getGraphQLParameters,
+  processRequest,
+  sendResult,
+} from 'graphql-helix';
+import express from 'express';
+import { createServer } from 'http';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      _: {
+        type: GraphQLString,
+        resolve: () => '_',
+      },
+    },
+  }),
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/graphql', async (req, res) => {
+  const request = {
+    body: req.body,
+    headers: req.headers,
+    method: req.method,
+    query: req.query,
+  };
+
+  const { operationName, query, variables } = getGraphQLParameters(request);
+
+  const result = await processRequest({
+    operationName,
+    query,
+    variables,
+    request,
+    schema,
+  });
+
+  sendResult(result, res);
+});
+
+app.listen(process.env.PORT);
