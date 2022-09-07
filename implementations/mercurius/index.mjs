b1699dada988a7837da0fef844be7e7d4443a115
Denis Badurina <denis@domonda.com>
2022-09-07 15:25:43 +0200

    mercurius

---
+++ implementations/mercurius/index.mjs
@@ -0,0 +1,21 @@
+import { GraphQLSchema, GraphQLString, GraphQLObjectType } from 'graphql';
+import Fastify from 'fastify';
+import mercurius from 'mercurius';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      _: {
+        type: GraphQLString,
+        resolve: () => '_',
+      },
+    },
+  }),
+});
+
+const app = Fastify();
+
+app.register(mercurius, { schema });
+
+app.listen({ port: process.env.PORT });
