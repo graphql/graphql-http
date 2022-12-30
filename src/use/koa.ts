e33ba1e06e855f36066390c3f40cdd6d0ac01044
xilesun <2013xile@gmail.com>
2022-12-30 16:35:19 +0800

    support integration with Koa

---
+++ src/use/koa.ts
@@ -0,0 +1,85 @@
+import type { Middleware } from 'koa';
+import type { IncomingMessage } from 'http';
+import {
+  createHandler as createRawHandler,
+  HandlerOptions,
+  OperationContext,
+} from '../handler';
+
+/**
+ * Create a GraphQL over HTTP Protocol compliant request handler for
+ * the Koa framework.
+ *
+ * ```js
+ * import Koa from 'koa'; // yarn add koa
+ * import mount from 'koa-mount'; // yarn add koa-mount
+ * import { createHandler } from 'graphql-http/lib/use/koa';
+ * import { schema } from './my-graphql-schema';
+ *
+ * const app = new Koa();
+ * app.use(mount('/', createHandler({ schema })));
+ *  
+ * app.listen({ port: 4000 });
+ * console.log('Listening to port 4000');
+ * ```
+ *
+ * @category Server/koa
+ */
+export function createHandler<Context extends OperationContext = undefined>(
+  options: HandlerOptions<IncomingMessage, undefined, Context>,
+): Middleware {
+  const isProd = process.env.NODE_ENV === 'production';
+  const handle = createRawHandler(options);
+  return async function requestListener(ctx) {
+    try {
+      const [body, init] = await handle({
+        url: ctx.url,
+        method: ctx.method,
+        headers: ctx.headers,
+        body: () => {
+          if (ctx.body) {
+            // in case koa has a body parser
+            return ctx.body;
+          }
+          return new Promise<string>((resolve) => {
+            let body = '';
+            ctx.req.on('data', (chunk) => (body += chunk));
+            ctx.req.on('end', () => resolve(body));
+          });
+        },
+        raw: ctx.req,
+        context: undefined,
+      });
+      ctx.body = body;
+      ctx.response.status = init.status;
+      ctx.response.message = init.statusText;
+      if (init.headers) {
+        for (const [name, value] of Object.entries(init.headers)) {
+          ctx.response.set(name, value);
+        }
+      }
+    } catch (err) {
+      // The handler shouldnt throw errors.
+      // If you wish to handle them differently, consider implementing your own request handler.
+      console.error(
+        'Internal error occurred during request handling. ' +
+          'Please check your implementation.',
+        err,
+      );
+      ctx.response.status = 500;
+      if (!isProd) {
+        ctx.response.set('content-type', 'application/json; charset=utf-8');
+        ctx.body = {
+          errors: [
+            err instanceof Error
+              ? {
+                  message: err.message,
+                  stack: err.stack,
+                }
+              : err,
+          ],
+        };
+      }
+    }
+  };
+}
