a40282310f450e48a54fefbebf90518e131d9841
enisdenjo <badurinadenis@gmail.com>
2023-02-12 18:15:58 +0100

    feat: Bundle the audits into UMD for browser usage

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,26 +2,51 @@ import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import gzip from 'rollup-plugin-gzip';
 
-export default {
-  input: './src/client.ts',
-  plugins: [typescript()],
-  output: [
-    {
-      file: './umd/graphql-http.js',
-      format: 'umd',
-      name: 'graphqlHttp',
-    },
-    {
-      file: './umd/graphql-http.min.js',
-      format: 'umd',
-      name: 'graphqlHttp',
-      plugins: [terser()],
-    },
-    {
-      file: './umd/graphql-http.min.js', // gzip plugin will add the .gz extension
-      format: 'umd',
-      name: 'graphqlHttp',
-      plugins: [terser(), gzip()],
-    },
-  ],
-};
+export default [
+  {
+    input: './src/client.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        file: './umd/graphql-http.js',
+        format: 'umd',
+        name: 'graphqlHttp',
+      },
+      {
+        file: './umd/graphql-http.min.js',
+        format: 'umd',
+        name: 'graphqlHttp',
+        plugins: [terser()],
+      },
+      {
+        file: './umd/graphql-http.min.js', // gzip plugin will add the .gz extension
+        format: 'umd',
+        name: 'graphqlHttp',
+        plugins: [terser(), gzip()],
+      },
+    ],
+  },
+  {
+    input: './src/audits/index.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        file: './umd/graphql-http-audits.js',
+        format: 'umd',
+        name: 'graphqlHttpAudits',
+      },
+      {
+        file: './umd/graphql-http-audits.min.js',
+        format: 'umd',
+        name: 'graphqlHttpAudits',
+        plugins: [terser()],
+      },
+      {
+        file: './umd/graphql-http-audits.min.js', // gzip plugin will add the .gz extension
+        format: 'umd',
+        name: 'graphqlHttpAudits',
+        plugins: [terser(), gzip()],
+      },
+    ],
+  },
+];
